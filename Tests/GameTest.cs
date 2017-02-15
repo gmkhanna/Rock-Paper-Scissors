b6b8e5b27caa15b921ecd98a4069b32fc0298d66
Minh Phuong and grinil khanna <minh+grinil@>
2017-02-15 08:53:55 -0800

    add next test to verify player 2 input, scissors returned

--- Tests/GameTest.cs
+++ Tests/GameTest.cs
@@ -6,11 +6,18 @@ namespace Game.Objects
   public class GameTest
   {
     [Fact]
-    public void stringTest1_RockReturn_Rock()
+    public void stringTestPlayer1_RockReturn_Rock()
     {
       Game testGame = new Game("Rock","Scissors");
-      string inputRock = "Rock";
-      Assert.Equal(testGame.GetP1(), inputRock);
+      string verifyInput = "Rock";
+      Assert.Equal(testGame.GetP1(), verifyInput);
+    }
+    [Fact]
+    public void stringTestPlayer2_RockReturn_Rock()
+    {
+      Game testGame = new Game("Rock","Scissors");
+      string verifyInput = "Scissors";
+      Assert.Equal(testGame.GetP2(), verifyInput);
     }
   }
 }
